2e243860f043486eb38cdcd4e00ee40d627e4717
LapsTime <39061148+LapsTimeOFF@users.noreply.github.com>
2021-09-30 22:25:41 +0200

    Updating

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main()
 {
      std::cout << "Hello World !" << std::endl;
      std::cout << "How are you ?" << std::endl;
-     printf("Hello form printf() Function !");
+     printf("Hello form printf() Function !\n");
 
      printf("Type a number : ");
      int number;
